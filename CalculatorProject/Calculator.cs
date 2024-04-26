71fcab9a1dd7e63a78c85f419e8ebf67bdb28570
jonathan wilkerson <jonathan.wilkerson@student.centralia.edu>
2024-04-25 21:27:01 -0700

    Solved Error TEst fix

--- CalculatorProject/Calculator.cs
+++ CalculatorProject/Calculator.cs
@@ -1,19 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.Design;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel.Design;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace CalculatorProject
+//namespace CalculatorProject
+//{
+//    public class Calculator
+//    {
+//        public int Add(int x, int y) { return x + y; }   
+//        public int Subtract (int x, int y) { return x - y; } 
+//        public int Multiply (int x, int y) { return x * y; }
+//        public int Divide(int x, int y) { return x / y; }
+
+
+//    }
+//}
+
+
+
+public class Calculator
 {
-    public class Calculator
+    public int Add(int x, int y)
     {
-        public int Add(int x, int y) { return x + y; }   
-        public int Subtract (int x, int y) { return x - y; } 
-        public int Multiply (int x, int y) { return x * y; }
-        public int Divide(int x, int y) { return x / y; }
-
-        
+        return x + y;
     }
+
+    // Other methods...
 }
--- CalculatorProjectTests/CalculatorProjectTests.cs
+++ CalculatorProjectTests/CalculatorProjectTests.cs
@@ -1,36 +1,61 @@
 
 
+//using CalculatorProject;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+//namespace CalculatorProjectTests
+//{
+//    [TestClass]
+//    public class CalculatorProjectTests
+//    {
+//        public TestContext? TestContext { get; set; } // TestContext is nullable
+
+//        [TestMethod]
+//        public void CalculatorProjectAdd_2_4_returns_6()
+//        {
+//            // Initialize x and y with default values
+//            int x = 0;
+//            int y = 0;
+
+//            // Safely parse TestContext properties if they are not null
+//            if (TestContext?.Properties["ValuX"] is string valuXStr)
+//            {
+//                x = int.Parse(valuXStr); // Direct parsing after null check
+//            }
+
+//            if (TestContext?.Properties["ValueY"] is string valueYStr)
+//            {
+//                y = int.Parse(valueYStr); // Direct parsing after null check
+//            }
+
+//            Calculator c = new Calculator();
+//            int result = c.Add(x, y);
+//            Assert.AreEqual(6, result); // The expected result should be 6 based on the test method's name
+//        }
+//    }
+//}
+
+
+
 using CalculatorProject;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace CalculatorProjectTests
+[TestClass]
+public class CalculatorProjectTests
 {
-    [TestClass]
-    public class CalculatorProjectTests
+    public TestContext TestContext { get; set; }
+
+    [TestMethod]
+    public void CalculatorProjectAdd_2_4_returns_6()
     {
-        public TestContext? TestContext { get; set; } // TestContext is nullable
+        // Initialize x and y with default values
+        int x = 2; // Assigning value 2 to x
+        int y = 4; // Assigning value 4 to y
 
-        [TestMethod]
-        public void CalculatorProjectAdd_2_4_returns_6()
-        {
-            // Initialize x and y with default values
-            int x = 0;
-            int y = 0;
+        // Instantiate the calculator
+        Calculator c = new Calculator();
+        int result = c.Add(x, y);
 
-            // Safely parse TestContext properties if they are not null
-            if (TestContext?.Properties["ValuX"] is string valuXStr)
-            {
-                x = int.Parse(valuXStr); // Direct parsing after null check
-            }
-
-            if (TestContext?.Properties["ValueY"] is string valueYStr)
-            {
-                y = int.Parse(valueYStr); // Direct parsing after null check
-            }
-
-            Calculator c = new Calculator();
-            int result = c.Add(x, y);
-            Assert.AreEqual(6, result); // The expected result should be 6 based on the test method's name
-        }
+        // Assert that the result is as expected
+        Assert.AreEqual(6, result, "The expected result should be 6 based on the test method's name");
     }
 }
